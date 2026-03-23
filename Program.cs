2dd19c3ec94240a0328a1ab49abc4c44983fd1de
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-03-23 12:20:39 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,30 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("====MENSAJE DE VOZ====");
-        //Variable
-        int opc;
-        //Crear el menu
-        Console.WriteLine("MENU PRINCIPAL");
-        Console.WriteLine("1- Recepcion");
-        Console.WriteLine("2- Vigilancia");
-        Console.WriteLine("3- Mantenimiento");
-        Console.WriteLine("4- Sali");
-        Console.WriteLine("Digite una opcion[ ]: ");
-        opc=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite una nota: ");
+        int nota = Convert.ToInt32 (Console.ReadLine());
 
-        //condicion
-        if (opc == 1)
-            Console.WriteLine("Bienvenido al Dpt. de recepcion");
-        if (opc == 2)
-            Console.WriteLine("Bienvenido al Dpt. de Vigilancia");
-        if (opc == 3)
-            Console.WriteLine("Bienvenido al Dpt. de Mantenimiento");
-        if (opc != 1 && opc != 2 && opc != 3)
-            Console.WriteLine("Saliendo del sistema");
-
-        //limpiar
-        Console.Clear();
-
+        if (nota >= 0 && nota < 60)
+            Console.WriteLine("Deficiente");
+        if (nota >= 60 && nota < 70)
+            Console.WriteLine("REgular");
+        if (nota >= 70 && nota < 80)
+            Console.WriteLine("Bien");
+        if (nota >= 80 && nota < 90)
+            Console.WriteLine("Muy bien ");
+        if (nota >= 90 && nota <= 100)
+            Console.WriteLine("Excelente");
     }
 }
