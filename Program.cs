7fadc67bcb74120136f02f2b2856bd9fe91d62dc
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-03-23 11:38:01 -0600

    Add files via upload
    Ejemplo if en casada C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("====MENSAJE DE VOZ====");
+        //Variable
+        int opc;
+        //Crear el menu
+        Console.WriteLine("MENU PRINCIPAL");
+        Console.WriteLine("1- Recepcion");
+        Console.WriteLine("2- Vigilancia");
+        Console.WriteLine("3- Mantenimiento");
+        Console.WriteLine("4- Sali");
+        Console.WriteLine("Digite una opcion[ ]: ");
+        opc=Convert.ToInt32(Console.ReadLine());
+
+        //condicion
+        if (opc == 1)
+            Console.WriteLine("Bienvenido al Dpt. de recepcion");
+        if (opc == 2)
+            Console.WriteLine("Bienvenido al Dpt. de Vigilancia");
+        if (opc == 3)
+            Console.WriteLine("Bienvenido al Dpt. de Mantenimiento");
+        if (opc != 1 && opc != 2 && opc != 3)
+            Console.WriteLine("Saliendo del sistema");
+
+        //limpiar
+        Console.Clear();
+
+    }
+}
