2585fcbc994bcfe35d0420aa5afe85b9286af804
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 21:22:18 -0600

    Add files via upload

---
+++ ProgramAct2_inciso1.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static int opcion;
+
+    private static void Main(string[] args)
+    {
+        //act2 lecc7 inciso1
+
+        Console.WriteLine("BUZON DE VOZ");
+        Console.WriteLine("1. Ventas");
+        Console.WriteLine("2. Recepcion");
+        Console.WriteLine("3. Direccion");
+        Console.WriteLine("4. Compras");
+        Console.WriteLine("Seleccione una opcion:");
+
+        opcion = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Bienvenido al departamento de Ventas");
+                break;
+
+            case 2:
+                Console.WriteLine("Bienvenido al departamento de Recepcion");
+                break;
+
+            case 3:
+                Console.WriteLine("Bienvenido al departamento de Direccion");
+                break;
+
+            case 4:
+                Console.WriteLine("Bienvenido al departamento de Compras");
+                break;
+
+            default:
+                Console.WriteLine("Opcion invalida");
+                break;
+        }
+    }
+}
