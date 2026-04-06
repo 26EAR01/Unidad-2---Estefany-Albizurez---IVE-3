59cad0c4d06271d4eed4d6b245cb09b31f3ce4a0
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 21:24:17 -0600

    Add files via upload

---
+++ ProgramAct3_inciso2.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Act3 lecc7 inciso2 
+
+        int ang1, ang2, ang3;
+
+        Console.WriteLine("Ingrese el angulo 1:");
+        ang1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el angulo 2:");
+        ang2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el angulo 3:");
+        ang3 = Convert.ToInt32(Console.ReadLine());
+
+        if (ang1 == ang2 && ang2 == ang3)
+        {
+            Console.WriteLine("Triangulo Equilatero");
+        }
+        else if (ang1 == ang2 || ang1 == ang3 || ang2 == ang3)
+        {
+            Console.WriteLine("Triangulo Isosceles");
+        }
+        else
+        {
+            Console.WriteLine("Triangulo Escaleno");
+        }
+    }
+}
