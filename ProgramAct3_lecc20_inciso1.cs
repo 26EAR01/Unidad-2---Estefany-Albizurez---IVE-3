48f4b09a5db766d542a9bf8d766ba8807a4f1485
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 22:05:55 -0600

    Add files via upload

---
+++ ProgramAct3_lecc20_inciso1.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Act3 lecc20 inciso1
+
+        int dia;
+
+        Console.WriteLine("Ingrese un numero del 1 al 7:");
+        dia = Convert.ToInt32(Console.ReadLine());
+
+        if (dia >= 1 && dia <= 5)
+        {
+            Console.WriteLine("Es un dia entre semana");
+        }
+        else if (dia == 6 || dia == 7)
+        {
+            Console.WriteLine("Es fin de semana");
+        }
+        else
+        {
+            Console.WriteLine("Numero invalido");
+        }
+    }
+}
