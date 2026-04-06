afb3aca4c31642c79fef7d2b5ec7b4ea9098327f
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 21:23:31 -0600

    Add files via upload

---
+++ ProgramAct3_inciso1.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //act3 lecc7 inciso1 
+
+        {
+            int dia, mes;
+
+            Console.WriteLine("Ingrese el dia:");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el mes (1-12):");
+            mes = Convert.ToInt32(Console.ReadLine());
+
+            if ((mes == 3 && dia >= 21) || (mes == 4) || (mes == 5) || (mes == 6 && dia <= 20))
+            {
+                Console.WriteLine("Primavera");
+            }
+            else if ((mes == 6 && dia >= 21) || (mes == 7) || (mes == 8) || (mes == 9 && dia <= 22))
+            {
+                Console.WriteLine("Verano");
+            }
+            else if ((mes == 9 && dia >= 23) || (mes == 10) || (mes == 11) || (mes == 12 && dia <= 20))
+            {
+                Console.WriteLine("Otoño");
+            }
+            else
+            {
+                Console.WriteLine("Invierno");
+            }
+        }
+    }
+}
