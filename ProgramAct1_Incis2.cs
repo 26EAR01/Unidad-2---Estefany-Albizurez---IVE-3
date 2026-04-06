cb3bab3019a974d6e676397e94d8fd2915c63a50
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 19:36:47 -0600

    Add files via upload

---
+++ ProgramAct1_Incis2.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Act1 lecc7 Inciso2
+
+        int edad;
+
+        Console.WriteLine("Ingrese su edad:");
+        edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Es mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Es menor de edad");
+        }
+    }
+}
