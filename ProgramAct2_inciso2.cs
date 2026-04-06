359387e02c9e4b606e3fec98c6fc90dcf8ffdf7b
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 21:22:43 -0600

    Add files via upload

---
+++ ProgramAct2_inciso2.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static int nota;
+
+    private static void Main(string[] args)
+    {
+        //Act2 lecc7 inciso2
+        Console.WriteLine("Ingrese la calificacion:");
+        nota = Convert.ToInt32(Console.ReadLine());
+
+        if (nota < 60)
+        {
+            Console.WriteLine("Deficiente");
+        }
+        else if (nota < 70)
+        {
+            Console.WriteLine("Regular");
+        }
+        else if (nota < 80)
+        {
+            Console.WriteLine("Bien");
+        }
+        else if (nota < 90)
+        {
+            Console.WriteLine("Muy Bien");
+        }
+        else
+        {
+            Console.WriteLine("Excelente");
+        }
+    }
+}
