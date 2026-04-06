dcf09ef934a4be3cc6650dcbf7a130f6239c7160
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 22:16:13 -0600

    Add files via upload

---
+++ ProgramAct3_Lecc20_Inciso2.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Act3 Lecc20 Inciso2
+
+            int nota;
+
+            Console.WriteLine("Ingrese la calificacion:");
+            nota = Convert.ToInt32(Console.ReadLine());
+
+            if (nota >= 60)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                Console.WriteLine("Reprobado");
+            }
+        }
+    }
+
