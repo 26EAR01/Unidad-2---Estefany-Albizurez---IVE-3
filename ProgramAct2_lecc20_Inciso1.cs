ecae6cd6ec514bdf8c91f8601df44fb66384b9c7
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 21:52:32 -0600

    Add files via upload

---
+++ ProgramAct2_lecc20_Inciso1.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Act2 lecc20 inciso1
+
+        string clave;
+
+        Console.WriteLine("Ingrese la contraseña:");
+        clave = Console.ReadLine();
+
+        if (clave == "Password123")
+        {
+            Console.WriteLine("Bienvenida");
+        }
+        else
+        {
+            Console.WriteLine("El ingreso esta prohibido");
+        }
+    }
+}
