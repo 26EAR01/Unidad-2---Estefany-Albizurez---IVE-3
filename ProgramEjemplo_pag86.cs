640fd8fad754190d03deb9948321079d7a291bf5
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 21:41:26 -0600

    Add files via upload

---
+++ ProgramEjemplo_pag86.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Ejemplo pag86 
+
+        int num = 0;
+
+        Console.WriteLine("Introduzca un numero:");
+
+        num = int.Parse(Console.ReadLine());
+
+        if (num < 0) { 
+            Console.WriteLine("El numero es menor a cero");
+        }
+        else { 
+            Console.WriteLine("El numero es igual o mayor a cero");
+        }
+    }
+}
