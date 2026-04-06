83283afbe71e2353bb99ff411139140d5295d714
26EAR01 Estefany Albizurez <albizurezestefany16db@gmail.com>
2026-04-05 19:32:05 -0600

    Add files via upload
    Practica C# Act1 Inciso1

---
+++ ProgramAct1_Incis1.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double nota1, nota2, nota3, nota4, nota5, promedio;
+
+        Console.WriteLine("Ingrese la nota 1:");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la nota 2:");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la nota 3:");
+        nota3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la nota 4:");
+        nota4 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la nota 5:");
+        nota5 = Convert.ToDouble(Console.ReadLine());
+
+        promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+        if (promedio >= 60)
+        {
+            Console.WriteLine("El alumno APRUEBA");
+        }
+        else
+        {
+            Console.WriteLine("El alumno REPRUEBA");
+
+        }
+    }
+}
